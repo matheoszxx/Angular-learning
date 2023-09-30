aae2b0f08e2fdb8149e0cff5af11e5d3807e8e75
matheoszxx <matheuszxx1@outlook.com>
2023-09-30 14:51:10 -0300

    '@Input()', 'OnInit' and 'OnChanges'

--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.css']
 })
-export class TitleComponent implements OnInit {
+export class TitleComponent implements OnInit, OnChanges {
 
-  constructor() { }
+	@Input() nome: string = ''
 
-  ngOnInit(): void {
-  }
+  constructor() {
+		console.log("Construtor");
 
+	}
+	//OnChanges está atrelado com o @Input (só ocorre a mundança se passar pelo input)
+	ngOnChanges(): void {
+		console.log("OnChange");
+
+	}
+
+	//lifeCycle = momento de vida inicial (primeira coisa que executa)
+	ngOnInit(): void {
+		console.log("OnInit");
+
+
+
+
+	}
 }
